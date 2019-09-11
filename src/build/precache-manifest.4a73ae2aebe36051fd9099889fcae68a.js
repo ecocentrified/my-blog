d1363675acaecc08595222854d7bf26d1550e4be
Justin <shapevibe@gmail.com>
2019-09-11 17:39:07 -0600

    First Commit

---
+++ src/build/precache-manifest.4a73ae2aebe36051fd9099889fcae68a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fec894b9eea62dc885f9e7413be49366",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db9e6ac85c9b51089831",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "623b6645468bf88ac080",
+    "url": "/static/js/2.99e789c6.chunk.js"
+  },
+  {
+    "revision": "db9e6ac85c9b51089831",
+    "url": "/static/js/main.9e58362b.chunk.js"
+  },
+  {
+    "revision": "eeab855a9420d0df9e27",
+    "url": "/static/js/runtime~main.a3235f1f.js"
+  }
+]);
